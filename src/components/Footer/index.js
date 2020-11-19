95eedc2ea4f511c42965aa645cd9ee3deed08486
Murilo-V <alveiramurilo@hotmail.com>
2020-11-19 20:00:02 -0300

    feat: finished footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import {Container} from './styles';
 import Logo from '../../assets/logo.svg'
 import iconLocation from '../../assets/icon-location.svg'
+import iconPhone from '../../assets/icon-phone.svg'
+import iconEmail from '../../assets/icon-email.svg'
+import { FaFacebookF, FaTwitter, FaInstagram } from 'react-icons/fa';
 
 const Footer = () => {
     return (
@@ -11,12 +14,36 @@ const Footer = () => {
             <div className="footer-container">
                 
                 <div className="location">
-                    <img src={iconLocation} alt="Fylo Logo" />
+                    <img src={iconLocation} alt="iconLocation" />
                     <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
+                </div>
+
+                <div className="contact">
+                    <p><img src={iconPhone} alt="iconPhone" /> +1-543-123-4567</p>
+                    <p><img src={iconEmail} alt="iconEmail" /> example@fylo.com</p>
+                </div>
+
+                <div className="footer-nav">
+                    <ul>
+                        <li>About Us</li>
+                        <li>Jobs</li>
+                        <li>Press</li>
+                        <li>Blog</li>
+                    </ul>
+                    <ul>
+                        <li>Contact Us</li>
+                        <li>Terms</li>
+                        <li>Privacy</li>
+                    </ul>
+                </div>
+
+                <div className="social-media">
+                    <FaFacebookF/>
+                    <FaTwitter />
+                    <FaInstagram />
                 </div>
             </div>
         </Container>
-
     )
 }
 
--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -8,30 +8,79 @@ export const Container = styled.footer`
     right: 0;
     padding: 80px 120px 40px 120px;
 
-    img {
-            user-select: none;
-  -moz-user-select: none;
-  -webkit-user-drag: none;
-  -webkit-user-select: none;
-  -ms-user-select: none;
-        }
-
         .logo {
             margin-bottom: 30px;
         }
 
         .footer-container {
             display: flex;
-            align-items: center;
+            align-items: top;
             justify-content: space-around;
+
+            div { margin:2%; }
 
             .location {
                 display: flex;
                 justify-content: space-between;
-                align-items: center;
+                align-items: top;
+
+                img {
+                    width: 15px;
+                    height: 20px;
+                }
 
                 p {
                     margin-left: 10px;
+                }
+            }
+
+            .contact {
+                display: flex;
+                align-items: left;
+                justify-content: left;
+                flex-direction: column;
+
+                p {
+                    display: flex;
+                align-items: left;
+                justify-content: left;
+                margin-bottom: 15px;
+                
+                    img {
+                        margin-right: 10px;
+                    }
+                }
+            }
+
+            .footer-nav {
+                display: flex;
+                align-items: top;
+                justify-content: center;
+                ul {
+                    margin-right: 20px;
+    
+                    li {
+                        list-style: none;
+                        margin-bottom: 20px;
+                    cursor: pointer;
+                        width: 90px;
+                    }
+                }
+            }
+
+            .social-media {
+                display: flex;
+                align-items: top;
+                justify-content: center;
+
+                svg {
+                    margin-left: 20px;
+                    width: 35px;
+                    height: 35px;
+                    border: 1px solid currentColor;
+                    border-radius: 40px;
+                    padding: 8px;
+                    cursor: pointer;
                 }
             }
         }
